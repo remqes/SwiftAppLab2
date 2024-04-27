20a674142452ce590ee124b72c8b5df248bd7779
Michał Nurzyński <michal.nurzynski@pollub.edu.pl>
2024-04-27 19:33:15 +0200

    koniec zajec

---
+++ SwiftAppLab2/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  SwiftAppLab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
