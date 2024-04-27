4d325d039758b396853ff860db1a54e7e583c19b
Michał Nurzyński <michal.nurzynski@pollub.edu.pl>
2024-04-27 18:25:09 +0200

    polecenie 3

--- SwiftAppLab2/CardView.swift
+++ SwiftAppLab2/CardView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct CardView: View {
+    let content: String
+    @State var isFaceUp: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        let roundedRectangle = RoundedRectangle(cornerRadius: 12)
+        
+        ZStack {
+            
+            Group {
+                roundedRectangle.fill(.white)
+                roundedRectangle
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2))
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            roundedRectangle
+                .fill(.blue)
+                .opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+        
     }
 }
 
 #Preview {
-    CardView()
+    CardView(content: "🎃")
 }
--- SwiftAppLab2/ContentView.swift
+++ SwiftAppLab2/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var cardCounter: Int = 2
+    let emojisFaces: Array<String> = ["🥹", "🙂", "😁", "🤒", "😵", "😦", "🤩", "😳", "😵‍💫", "🫨"]
+    let emojisAnimals: Array<String> = ["🦣", "🐁", "🦉", "🦧", "🐈", "🦚", "🦢", "🐥", "🐋", "🐊"]
+    let emojisOther: Array<String> = ["🕸️", "🥝", "🥦", "🥩", "🥨", "🥫", "🥕", "🥑", "🥥", "🧅"]
+    
+    //BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("hello, world2!")
+        
+        cardDisplay
+        buttonsView
+        
+    }
+    
+    var buttonsView: some View {
+        HStack {
+            adjustCardNumber(by: 2, symbol: "plus.square.fill")
+                .padding()
+            Spacer()
+            adjustCardNumber(by: -2, symbol: "minus.square.fill")
+                .padding()
         }
-        .padding()
+    }
+    
+    var cardDisplay: some View {
+        ScrollView {
+            LazyVGrid(
+                columns: [GridItem(.adaptive(minimum: 160))
+            ]) {
+                ForEach(0 ..< cardCounter, id: \.self) {
+                    i in CardView(content: emojisFaces[i])
+                }
+            }
+            .foregroundColor(.blue)
+            .padding()
+        }
+    }
+    
+    func adjustCardNumber(by offset: Int, symbol: String) -> some View {
+        Button(
+            action: {
+                cardCounter += offset
+            }, 
+            label: {
+                Image(systemName: symbol).font(.largeTitle)
+            })
+        .disabled(
+            cardCounter + offset < 2 || 
+            cardCounter + offset > emojisFaces.count
+        )
     }
 }
 
