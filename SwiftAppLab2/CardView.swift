ba09f7ada93d5b3182c8f2f9bef17f62c14bd1e0
Michał Nurzyński <michal.nurzynski@pollub.edu.pl>
2024-04-27 17:48:17 +0200

    polecenie 1

---
+++ SwiftAppLab2/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  SwiftAppLab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
